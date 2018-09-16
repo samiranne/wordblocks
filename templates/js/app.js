7e55e13bc0bddaebfcd7a27204b3a42cbae64d75
Zack M. Davis <code@zackmdavis.net>
2018-09-15 18:39:53 -0700

    loop over categories to set event-listeners
    Since we're "doing the same thing" for all three word categories, we
    can express that more concisely with a loop. I'm assuming that we
    don't care about Internet Explorer compatibility (Merlin deserves only
    the finest modern browesers), so I've felt free to use `let`, `const`,
    and `NodeList.forEach`
    (https://developer.mozilla.org/en-US/docs/Web/API/NodeList).

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,23 +1,12 @@
-var subjects = document.querySelectorAll('.subject');
-Array.prototype.forEach.call(subjects, function(el, i){
-    el.addEventListener('click', function() {
-        document.getElementById('subject').innerHTML = el.innerHTML;
+let categories = ['subject', 'verb', 'predicate'];
+for (const category of categories) {
+    let words = document.querySelectorAll(`.${category}`);
+    words.forEach(function(el, i) {
+        el.addEventListener('click', function() {
+            document.getElementById(category).innerHTML = el.innerHTML;
+        });
     });
-});
-
-var verbs = document.querySelectorAll('.verb');
-Array.prototype.forEach.call(verbs, function(el, i){
-    el.addEventListener('click', function() {
-        document.getElementById('verb').innerHTML = el.innerHTML;
-    });
-});
-
-var predicates = document.querySelectorAll('.predicate');
-Array.prototype.forEach.call(predicates, function(el, i){
-    el.addEventListener('click', function() {
-        document.getElementById('predicate').innerHTML = el.innerHTML;
-    });
-});
+}
 
 document.querySelectorAll('.wordbank div').forEach((a)=>a.onclick = ()=> {
     var soundEnabled = document.getElementById('sound_enabled').checked;
